2eeccc9faefb5c01e16a915cd116a396a6f557dd
shahab-github <shahabuddinm679+github@gmail.com>
2024-01-07 17:26:20 -0600

    feat: added the hello.go

---
+++ hello.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+const (
+	french  = "French"
+	spanish = "Spanish"
+
+	englishHelloPrefix = "Hello, "
+	spanishHelloPrefix = "Hola, "
+	frenchHelloPrefix  = "Bonjour, "
+)
+
+func Hello(name, language string) string {
+	if name == "" {
+		name = "world"
+	}
+
+	return greetingPrefix(language) + name
+}
+
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case french:
+		prefix = frenchHelloPrefix
+	case spanish:
+		prefix = spanishHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return
+}
+
+func main() {
+	fmt.Println(Hello("Chris", ""))
+}
