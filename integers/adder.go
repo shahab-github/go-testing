3d1b96bba7cb62fb08efc8e93ecd9361c0be3c5a
shahab-github <shahabuddinm679+github@gmail.com>
2024-01-08 18:41:58 -0600

    refactor: now using append method

--- integers/adder.go
+++ integers/adder.go
@@ -14,12 +14,21 @@ func Sum(numbers []int) int {
 	return result
 }
 
-func SumAll(numbersToSum ...[]int) []int {
-	lengthOfNumbers := len(numbersToSum)
-	sums := make([]int, lengthOfNumbers)
+// func SumAll(numbersToSum ...[]int) []int {
+// 	lengthOfNumbers := len(numbersToSum)
+// 	sums := make([]int, lengthOfNumbers)
 
-	for i, numbers := range numbersToSum {
-		sums[i] = Sum(numbers)
+// 	for i, numbers := range numbersToSum {
+// 		sums[i] = Sum(numbers)
+// 	}
+// 	return sums
+// }
+
+func SumAll(numbersToSum ...[]int) []int {
+	var sums []int
+
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
 	}
 	return sums
 }
