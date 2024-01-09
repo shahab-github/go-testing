e7ac0169e5c5c053b69a5202fc28df27947f7e01
shahab-github <shahabuddinm679+github@gmail.com>
2024-01-08 20:13:44 -0600

    test: updated the unit test

--- structs_methods_interfaces/shapes_test.go
+++ structs_methods_interfaces/shapes_test.go
@@ -11,7 +11,7 @@ import "testing"
 
 func TestPerimeter(t *testing.T) {
 	rectangle := Rectangle{10.0, 10.0}
-	got := Perimeter(rectangle)
+	got := rectangle.Perimeter()
 	want := 40.0
 
 	if got != want {
@@ -19,12 +19,35 @@ func TestPerimeter(t *testing.T) {
 	}
 }
 
-func TestArea(t *testing.T) {
-	rectangle := Rectangle{12.0, 6.0}
-	got := Area(rectangle)
-	want := 72.0
+// func TestArea(t *testing.T) {
+// 	rectangle := Rectangle{12.0, 6.0}
+// 	got := Area(rectangle)
+// 	want := 72.0
 
-	if got != want {
-		t.Errorf("got %.2f, want %.2f", got, want)
-	}
+// 	if got != want {
+// 		t.Errorf("got %.2f, want %.2f", got, want)
+// 	}
+// }
+
+func TestArea(t *testing.T) {
+
+	t.Run("rectangle", func(t *testing.T) {
+		rect := Rectangle{10.0, 10.0}
+		got := rect.Area()
+		want := 100.0
+
+		if got != want {
+			t.Errorf("got %.2f but want %.2f", got, want)
+		}
+	})
+
+	t.Run("circle", func(t *testing.T) {
+		circle := Circle{10}
+		got := circle.Area()
+		want := 314.0
+
+		if got != want {
+			t.Errorf("got %.2f but want %.2f", got, want)
+		}
+	})
 }
