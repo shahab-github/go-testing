ef8e2b03cb4b18bcb00abf94fef01fc3e0238a3c
shahab-github <shahabuddinm679+github@gmail.com>
2024-01-08 18:34:25 -0600

    test: add unit test for SumAll()

--- integers/adder_test.go
+++ integers/adder_test.go
@@ -40,3 +40,12 @@ func TestSum(t *testing.T) {
 		}
 	})
 }
+
+func TestSumAll(t *testing.T) {
+	got := SumAll([]int{1, 2}, []int{0, 9})
+	want := []int{3, 9}
+
+	if got != want {
+		t.Errorf("got %v, want %v", got, want)
+	}
+}
