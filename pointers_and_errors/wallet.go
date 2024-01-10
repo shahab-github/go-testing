cf9ed0035ebf91945c76917bb13d92dcc36f9bd9
shahab-github <shahabuddinm679+github@gmail.com>
2024-01-09 23:02:23 -0600

    fix: using pointer

--- pointers_and_errors/wallet.go
+++ pointers_and_errors/wallet.go
@@ -4,10 +4,10 @@ type Wallet struct {
 	balance int
 }
 
-func (w Wallet) Deposit(amount int) {
+func (w *Wallet) Deposit(amount int) {
 	w.balance += amount
 }
 
-func (w Wallet) Balance() int {
+func (w *Wallet) Balance() int {
 	return w.balance
 }
