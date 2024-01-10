d9e365a18be6479f1e2c374355a8ba14e1b7769f
shahab-github <shahabuddinm679+github@gmail.com>
2024-01-09 23:05:31 -0600

    fix: failing unit test

--- pointers_and_errors/wallet_test.go
+++ pointers_and_errors/wallet_test.go
@@ -5,9 +5,9 @@ import "testing"
 func TestWallet(t *testing.T) {
 	wallet := Wallet{}
 
-	wallet.Deposit(10)
+	wallet.Deposit(Bitcoin(10))
 	got := wallet.Balance()
-	want := 10
+	want := Bitcoin(10)
 
 	if got != want {
 		t.Errorf("got %d, want %d", got, want)
