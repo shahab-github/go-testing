d71001f0aceaae3bf716dfab6b14e10be55d4603
shahab-github <shahabuddinm679+github@gmail.com>
2024-01-09 23:14:25 -0600

    test: add unit test for withdraw

--- pointers_and_errors/wallet_test.go
+++ pointers_and_errors/wallet_test.go
@@ -2,14 +2,42 @@ package mypointer
 
 import "testing"
 
+// func TestWallet(t *testing.T) {
+// 	wallet := Wallet{}
+
+// 	wallet.Deposit(Bitcoin(10))
+// 	got := wallet.Balance()
+// 	want := Bitcoin(10)
+
+// 	if got != want {
+// 		t.Errorf("got %s, want %s", got, want)
+// 	}
+// }
+
+// refactor to
 func TestWallet(t *testing.T) {
-	wallet := Wallet{}
 
-	wallet.Deposit(Bitcoin(10))
-	got := wallet.Balance()
-	want := Bitcoin(10)
+	t.Run("deposit", func(t *testing.T) {
+		wallet := Wallet{}
 
-	if got != want {
-		t.Errorf("got %d, want %d", got, want)
-	}
+		wallet.Deposit(Bitcoin(10))
+		got := wallet.Balance()
+		want := Bitcoin(10)
+
+		if got != want {
+			t.Errorf("got %s, want %s", got, want)
+		}
+	})
+
+	t.Run("withdraw", func(t *testing.T) {
+		wallet := Wallet{balance: Bitcoin(10)}
+
+		wallet.Withdraw(Bitcoin(10))
+		got := wallet.Balance()
+		want := Bitcoin(10)
+
+		if got != want {
+			t.Errorf("got %s, want %s", got, want)
+		}
+	})
 }
