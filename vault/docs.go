cc460875c5ead8b66bfd9151c221ab740d6ba682
shahab-github <113800863+shahab-github@users.noreply.github.com>
2024-03-22 07:49:17 -0500

    Update docs.go

--- vault/docs.go
+++ vault/docs.go
@@ -1,1 +1,4 @@
 ### Vault AWS secret Engine: https://developer.hashicorp.com/vault/docs/secrets/aws
+
+### Dynamic Credentials with the Vault Provider
+https://developer.hashicorp.com/terraform/cloud-docs/workspaces/dynamic-provider-credentials/vault-configuration?ajs_aid=8fa3821e-e2d1-4505-aea7-f79525b4bc61&product_intent=terraform#dynamic-credentials-with-the-vault-provider
